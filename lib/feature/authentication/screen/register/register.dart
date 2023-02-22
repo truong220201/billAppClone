65a517b56dc92e5f2b08fc3d39bb566585970390
truongls <truongls@imt-soft.com>
2023-02-22 10:22:06 +0700

    fix

---
+++ lib/feature/authentication/screen/register/register.dart
@@ -0,0 +1,412 @@
+import 'package:bill_app/feature/authentication/screen/login/login.dart';
+import 'package:bill_app/main.dart';
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  final Color? primaryColor;
+  final Color? backgroundColor;
+  final AssetImage backgroundImage;
+
+  const RegisterScreen({
+    super.key,
+    this.primaryColor = Colors.green,
+    this.backgroundColor = Colors.white,
+    this.backgroundImage = const AssetImage("assets/images/full-bloom.png"),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            color: backgroundColor,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Expanded(
+                child: ClipPath(
+                  clipper: MyClipper(),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: backgroundImage,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.only(top: 100.0, bottom: 100.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "BILL",
+                          style: TextStyle(
+                              fontSize: 50.0,
+                              fontWeight: FontWeight.bold,
+                              color: primaryColor),
+                        ),
+                        Text(
+                          "Register",
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              color: primaryColor),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 40.0),
+                child: Text(
+                  "Email",
+                  style: TextStyle(color: Colors.grey, fontSize: 16.0),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey.withOpacity(0.5),
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 15.0),
+                      child: Icon(
+                        Icons.person_outline,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Container(
+                      height: 30.0,
+                      width: 1.0,
+                      color: Colors.grey.withOpacity(0.5),
+                      margin: const EdgeInsets.only(left: 00.0, right: 10.0),
+                    ),
+                    const Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Enter your email',
+                          hintStyle: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 40.0),
+                child: Text(
+                  "User Name",
+                  style: TextStyle(color: Colors.grey, fontSize: 16.0),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey.withOpacity(0.5),
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 15.0),
+                      child: Icon(
+                        Icons.person_outline,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Container(
+                      height: 30.0,
+                      width: 1.0,
+                      color: Colors.grey.withOpacity(0.5),
+                      margin: const EdgeInsets.only(left: 00.0, right: 10.0),
+                    ),
+                    const Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'User name',
+                          hintStyle: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 40.0),
+                child: Text(
+                  "Password",
+                  style: TextStyle(color: Colors.grey, fontSize: 16.0),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey.withOpacity(0.5),
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 15.0),
+                      child: Icon(
+                        Icons.lock_open,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Container(
+                      height: 30.0,
+                      width: 1.0,
+                      color: Colors.grey.withOpacity(0.5),
+                      margin: const EdgeInsets.only(left: 00.0, right: 10.0),
+                    ),
+                    const Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Enter your password',
+                          hintStyle: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 40.0),
+                child: Text(
+                  "Re-Password",
+                  style: TextStyle(color: Colors.grey, fontSize: 16.0),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey.withOpacity(0.5),
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 15.0),
+                      child: Icon(
+                        Icons.lock_open,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Container(
+                      height: 30.0,
+                      width: 1.0,
+                      color: Colors.grey.withOpacity(0.5),
+                      margin: const EdgeInsets.only(left: 00.0, right: 10.0),
+                    ),
+                    const Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Enter your re-password',
+                          hintStyle: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
+                          //splashColor: this.primaryColor,
+                          backgroundColor: primaryColor,
+                          primary: Colors.black12,
+                          foregroundColor: primaryColor,
+                        ),
+                        child: Row(
+                          children: <Widget>[
+                            const Padding(
+                              padding: EdgeInsets.only(left: 20.0),
+                              child: Text(
+                                "Register",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(),
+                            ),
+                            Transform.translate(
+                              offset: const Offset(0.0, 0.0),
+                              child: Container(
+                                padding: const EdgeInsets.all(5.0),
+                                child: TextButton(
+                                  style: TextButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(28.0)),
+                                    //splashColor: Colors.white,
+                                    backgroundColor: Colors.white,
+                                    foregroundColor: Colors.white,
+                                  ),
+                                  child: Icon(
+                                    Icons.arrow_forward,
+                                    color: primaryColor,
+                                  ),
+                                  onPressed: () => {},
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        onPressed: () => {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LoginScreen()),
+                          )
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // Container(
+              //   margin: const EdgeInsets.only(top: 10.0),
+              //   padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              //   child: Row(
+              //     children: <Widget>[
+              //       Expanded(
+              //         child: TextButton(
+              //           style: TextButton.styleFrom(
+              //             shape: RoundedRectangleBorder(
+              //                 borderRadius: BorderRadius.circular(30.0)),
+              //             //splashColor: Color(0xFF3B5998),
+              //             foregroundColor: const Color(0xff3B5998),
+              //           ),
+              //           child: Row(
+              //             children: <Widget>[
+              //               const Padding(
+              //                 padding: EdgeInsets.only(left: 20.0),
+              //                 child: Text(
+              //                   "Register WITH FACEBOOK",
+              //                   style: TextStyle(color: Colors.white),
+              //                 ),
+              //               ),
+              //               Expanded(
+              //                 child: Container(),
+              //               ),
+              //               Transform.translate(
+              //                 offset: const Offset(15.0, 0.0),
+              //                 child: Container(
+              //                   padding: const EdgeInsets.all(5.0),
+              //                   child: TextButton(
+              //                     style: TextButton.styleFrom(
+              //                       shape: RoundedRectangleBorder(
+              //                           borderRadius:
+              //                               BorderRadius.circular(28.0)),
+              //                       //splashColor: Colors.white,
+              //                       foregroundColor: Colors.white,
+              //                     ),
+              //                     child: const Icon(
+              //                       IconData(0xea90, fontFamily: 'icomoon'),
+              //                       color: Color(0xff3b5998),
+              //                     ),
+              //                     onPressed: () => {},
+              //                   ),
+              //                 ),
+              //               )
+              //             ],
+              //           ),
+              //           onPressed: () => {},
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // ),
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
+                          foregroundColor: Colors.transparent,
+                        ),
+                        child: Container(
+                          padding: const EdgeInsets.only(left: 20.0),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "DON'T HAVE AN ACCOUNT?",
+                            style: TextStyle(color: primaryColor),
+                          ),
+                        ),
+                        onPressed: () => {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path p = Path();
+    p.lineTo(size.width, 0.0);
+    p.lineTo(size.width, size.height * 0.85);
+    p.arcToPoint(
+      Offset(0.0, size.height * 0.85),
+      radius: const Radius.elliptical(50.0, 10.0),
+      rotation: 0.0,
+    );
+    p.lineTo(0.0, 0.0);
+    p.close();
+    return p;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) {
+    return true;
+  }
+}
